e083b1783f64e60eef3d88b971f3bc963f9dace4
fabianosalles <fabiano.salles@gmail.com>
2024-12-15 16:16:03 -0300

    feat - initial migration added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 77-tech-test
+
+## Database Migrations
+
+To init the database, run the following command and the `LMS.BookInventory.Infra` directory
+
+```shell
+ dotnet ef migrations add Initial -s ../LMS.BookInventory.Api/ --context LMS.BookInventory.Infra.Database.BookContext -o /Database/Migrations
+ ```
+
+ To update the database...
+
+ ```shell
+ dotnet ef database update
+ ```
--- src/LMS.BookInventory.Infra/Database/BookContext.cs
+++ src/LMS.BookInventory.Infra/Database/BookContext.cs
@@ -1,9 +1,24 @@
 ﻿using LMS.BookInventory.Infra.Database.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace LMS.BookInventory.Infra.Database;
     
 public class BookContext: DbContext
 {
+    public BookContext(DbContextOptions<BookContext> options) : base(options){}
+
     public virtual DbSet<Book> Books { get; set; }
 }
+
+/*
+public class BookContextDesignFactory : IDesignTimeDbContextFactory<BookContext>
+{
+    public BookContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<BookContext>();
+        optionsBuilder.UseSqlite("Data Source=blog.db");
+
+        return new BookContext(optionsBuilder.Options);
+    }
+}*/
