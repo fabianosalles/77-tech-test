5d46c04554f06e21baf840e97059733fae653b26
fabianosalles <fabiano.salles@gmail.com>
2024-12-15 16:00:26 -0300

    feat - preparing for migrations

---
+++ src/LMS.BookInventory.Api/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using LMS.BookInventory.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.BookInventory.Controllers;
+
+[ApiController]
+[Route("api/v{version:ApiVersions.V1}/[controller]")]
+public class BooksController: ControllerBase
+{
+    public BooksController(){}
+
+    public ActionResult Ping()
+    {
+        return Ok("Pong");
+    }
+}
---
+++ src/LMS.BookInventory.Api/Models/ApiVersions.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.BookInventory.Models;
+
+/// <summary>
+/// ApiVersion class holds a list of all supported API versions.
+/// </summary>
+public static class ApiVersions
+{
+    /// <summary>
+    /// Version 1 of the API.
+    /// </summary>
+    public const string V1 = "1";
+}
