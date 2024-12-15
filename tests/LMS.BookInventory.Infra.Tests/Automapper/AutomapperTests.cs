55faf2981e71d621ec4879e6176b11d443445826
fabianosalles <fabiano.salles@gmail.com>
2024-12-15 16:30:57 -0300

    feat - unit tests for automapper on infra layer

---
+++ tests/LMS.BookInventory.Infra.Tests/Automapper/AutomapperTests.cs
@@ -0,0 +1,18 @@
+﻿using LMS.BookInventory.Infra.UnitTests.Helpers;
+
+namespace LMS.BookInventory.Infra.UnitTests.Automapper;
+
+public class AutomapperTests
+{
+    [Fact]
+    public void Mapper_ConfigurationIsValid()
+    {
+        // Arrange & Act
+        var mapper = AutomapperLoader.LoadInfrastructure();
+
+        // Assert
+        // This validates all properties on models defined in a mapping profile are mapped 
+        // or otherwise accounted for
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
---
+++ tests/LMS.BookInventory.Infra.Tests/Helpers/AutomapperLoader.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace LMS.BookInventory.Infra.UnitTests.Helpers;
+
+/// <summary>
+/// Loads automapper from assembly
+/// </summary>
+public static class AutomapperLoader
+{
+    /// <summary>
+    /// Loads automapper configuration from infrastructure assemblies 
+    /// </summary>
+    /// <returns></returns>
+    public static IMapper LoadInfrastructure()
+    {
+        const string infraAssembly = "LMS.BookInventory.Infra";
+
+        var mapper = new MapperConfiguration(cfg => { cfg.AddMaps(infraAssembly); }).
+                CreateMapper();
+
+        return mapper;
+    }
+}
